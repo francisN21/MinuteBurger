f36204aa6b26142fd9fded8ad19232f1ed3188ed
Francisco Rones <ininorones@gmail.com>
2021-01-18 19:34:54 -0800

    Addition: New file structure added for the handlebars

--- models/burger.js
+++ models/burger.js
@@ -5,13 +5,15 @@ const burger = {
   all(cb) {
     orm.all("burgers", (res) => cb(res));
   },
-  create(cb) {
-    orm.all("burgers", (res) => cb(res));
+  create(cols, vals, cb) {
+    orm.create("burgers", cols, vals, (res) => cb(res));
   },
-  update(cb) {
-    orm.all("burgers", (res) => cb(res));
+  update(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, (res) => cb(res));
   },
-  delete(cb) {
-    orm.all("burgers", (res) => cb(res));
+  delete(condition, cb) {
+    orm.all("burgers", condition, (res) => cb(res));
   },
 };
+
+module.exports = burger;
