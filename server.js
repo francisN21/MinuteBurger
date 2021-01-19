bc6024e738e84205d95127e48fb4f78c107859ff
Francisco Rones <ininorones@gmail.com>
2021-01-18 17:09:09 -0800

    Update: server.js setup complete and running

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// needs the JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: https://localhost:${PORT}`)
+);
