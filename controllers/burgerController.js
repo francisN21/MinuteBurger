affe4f2ca5c4c34bd94dadc6d48fb3c80c741fb3
Francisco Rones <ininorones@gmail.com>
2021-01-18 18:44:19 -0800

    Update: redoing the commit

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 router.get("/", (req, res) => {
+  //   use the ORM funtion to render burgers to be devoured on the screen
   burger.all((data) => {
     const hbsObject = {
       burgers: data,
@@ -14,7 +15,15 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/api/burger", (req, res) => {});
+router.post("/api/burger", (req, res) => {
+  burger.create(
+    ["burger_name", "devour"],
+    [req.body.name, req.body.devoured],
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
+});
 
 router.put("/api/burger/:id", (req, res) => {});
 
